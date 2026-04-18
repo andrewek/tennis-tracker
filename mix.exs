8a95a99d0473742cd6ccdd1ea23181ee3e11628a
Andrew Ek <andrewek@gmail.com>
2026-04-17 22:22:47 -0500

    Add Credo

--- mix.exs
+++ mix.exs
@@ -55,6 +55,7 @@ defmodule TennisTracker.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.1.0"},
       {:lazy_html, ">= 0.1.0", only: :test},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
       {:sourceror, "~> 1.7", only: [:dev, :test]},
@@ -102,7 +103,13 @@ defmodule TennisTracker.MixProject do
         "esbuild tennis_tracker --minify",
         "phx.digest"
       ],
-      precommit: ["compile --warnings-as-errors", "deps.unlock --unused", "format", "test"]
+      precommit: [
+        "compile --warnings-as-errors",
+        "deps.unlock --unused",
+        "format",
+        "test",
+        "credo"
+      ]
     ]
   end
 
